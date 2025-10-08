641d852e14b993ecb07a62ee217e9a6af905a851
Jatin6396 <jatinagrawal1704@gmail.com>
2025-10-08 11:37:34 +0530

    Day9

---
+++ Day9/q2.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public static ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    public ListNode deleteDuplicates(ListNode head) {
+    LinkedHashSet<Integer> hs=new LinkedHashSet<>();
+     ListNode temp=head;
+     while(temp!=null){
+        hs.add(temp.val);
+        temp=temp.next;
+     }
+     temp=new ListNode(-1);
+     ListNode tail=temp;
+
+     for (int val : hs) {  
+    tail.next = new ListNode(val); 
+    tail = tail.next;             
+     }
+
+    // head=reverse(temp.next);
+     return temp.next;
+    
+    }
+}
+
+/*  --------------------------------2nd sol
+ * /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ 
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        if(head==null ) return head;
+        ListNode curr=head;
+        ListNode prev=null;
+
+        while(curr.next!=null){
+            prev=curr;
+            curr=curr.next;
+           if(prev.val==curr.val){
+            prev.next=curr.next;
+            curr=prev;
+           }
+           
+        }
+
+        return head;
+    }
+}
+ */
